49fe590141c3b1c280f374a5ad97b2b60443ba00
kalyan394 <68234658+kalyan394@users.noreply.github.com>
2022-08-30 13:14:06 +0200

    Update 2_gazebo_spawn.launch.py

--- ROS/rover/launch/2_gazebo_spawn.launch.py
+++ ROS/rover/launch/2_gazebo_spawn.launch.py
@@ -9,21 +9,21 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
   urdf = os.path.join(get_package_share_directory('rover'),'rover.urdf')
   return LaunchDescription([
-    #   publishes TF for links of the robot without joints
+    
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
             arguments=[urdf]),
-    #  To publish tf for Joints only links
+   
         Node(
             package='joint_state_publisher',
             executable='joint_state_publisher',
             name='joint_state_publisher',
             output='screen',
             ),
-#  Gazebo related stuff required to launch the robot in simulation
+
         ExecuteProcess(
             cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
             output='screen'),
